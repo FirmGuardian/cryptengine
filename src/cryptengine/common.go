1df127f9d511b516508fbcf80da4615c561a533a
Liam Conrad <liam@storskegg.org>
2017-11-14 17:18:47 -0700

    code cleanup, remove redundant type declarations

--- src/cryptengine/common.go
+++ src/cryptengine/common.go
@@ -25,9 +25,9 @@ func (t ErrType) code() int {
 
 // Universal constants
 const (
-	constPassphrase         string = ""
-	legalCryptFileExtension string = ".lcsf"
-	maxInputFileSize        int64  = 1024 * 1024 * 512 // 512MB; uint64 to support >= 4GB
+	constPassphrase               = ""
+	legalCryptFileExtension       = ".lcsf"
+	maxInputFileSize        int64 = 1024 * 1024 * 512 // 512MB; uint64 to support >= 4GB
 )
 
 var appRoot = map[string]string{
@@ -37,34 +37,34 @@ var appRoot = map[string]string{
 
 // Error Definitions, all in one spot
 var errs = map[string]ErrType{
-	"fsCantOpenFile":                    ErrType{200, "Unable to access file"},
-	"fsCantCreateFile":                  ErrType{201, "Unable to create file"},
-	"fsCantDeleteFile":                  ErrType{202, "Unable to remove existing file"},
-	"ioCantReadFromFile":                ErrType{300, "Unable to read from file"},
-	"memFileTooBig":                     ErrType{400, "Input file exceeds maximum"},
-	"cryptCantReadEncryptedBlock":       ErrType{500, "Error reading encryptedData"},
-	"cryptCantDeserializeEncryptedData": ErrType{501, "Unable to deserialize encrypted data"},
-	"cryptCantDecodePrivatePEM":         ErrType{501, "Failed to decode PEM block of private key"},
-	"cryptCantDecodePublicPEM":          ErrType{502, "Failed to decode PEM block of public key"},
-	"cryptCantDecryptPrivateBlock":      ErrType{503, "Unable to decrypt private block"},
-	"cryptCantParsePrivateKey":          ErrType{504, "Unable to parse decrypted private key"},
-	"cryptCantParsePublicKey":           ErrType{505, "Unable to parse public key"},
-	"cryptCantEncryptZip":               ErrType{510, "Unable to encrypt generated zip archive"},
-	"cryptCantEncryptOrWrite":           ErrType{511, "Unable to encrypt data, or write encrypted file!"},
-	"cryptCantDecryptCipher":            ErrType{520, "Unable to decrypt cipher"},
-	"cryptCantDecryptFile":              ErrType{521, "Unable to decrypt file"},
-	"cryptAESCantCreateCipher":          ErrType{550, "Unable to create AES cipher"},
-	"cryptAESCantCreateGCMBlock":        ErrType{551, "Unable to create GCM Block"},
-	"cryptAESCantDecrypt":               ErrType{552, "Unable to decrypt data"},
-	"cryptAESCantGenerateSessionKey":    ErrType{553, "Unable to generate sessionKey"},
-	"keypairCantReadPublicKey":          ErrType{600, "Error reading public key"},
-	"keypairCantReadPrivateKey":         ErrType{601, "Error reading private key"},
-	"keypairCantGeneratePrivateKey":     ErrType{602, "Failed to generate private key"},
-	"keypairCantValidatePrivateKey":     ErrType{603, "Failed to validate private key"},
-	"keypairCantEncryptPrivatePEM":      ErrType{604, "Failed to encrypt private PEM"},
-	"keypairCantMarshalPublicKey":       ErrType{605, "Failed to marshal public key block"},
-	"panicBadErrType":                   ErrType{1000, "Bad ErrType"},
-	"panicWTF":                          ErrType{1001, "Something has gone horribly wrong."},
+	"fsCantOpenFile":                    {200, "Unable to access file"},
+	"fsCantCreateFile":                  {201, "Unable to create file"},
+	"fsCantDeleteFile":                  {202, "Unable to remove existing file"},
+	"ioCantReadFromFile":                {300, "Unable to read from file"},
+	"memFileTooBig":                     {400, "Input file exceeds maximum"},
+	"cryptCantReadEncryptedBlock":       {500, "Error reading encryptedData"},
+	"cryptCantDeserializeEncryptedData": {501, "Unable to deserialize encrypted data"},
+	"cryptCantDecodePrivatePEM":         {501, "Failed to decode PEM block of private key"},
+	"cryptCantDecodePublicPEM":          {502, "Failed to decode PEM block of public key"},
+	"cryptCantDecryptPrivateBlock":      {503, "Unable to decrypt private block"},
+	"cryptCantParsePrivateKey":          {504, "Unable to parse decrypted private key"},
+	"cryptCantParsePublicKey":           {505, "Unable to parse public key"},
+	"cryptCantEncryptZip":               {510, "Unable to encrypt generated zip archive"},
+	"cryptCantEncryptOrWrite":           {511, "Unable to encrypt data, or write encrypted file!"},
+	"cryptCantDecryptCipher":            {520, "Unable to decrypt cipher"},
+	"cryptCantDecryptFile":              {521, "Unable to decrypt file"},
+	"cryptAESCantCreateCipher":          {550, "Unable to create AES cipher"},
+	"cryptAESCantCreateGCMBlock":        {551, "Unable to create GCM Block"},
+	"cryptAESCantDecrypt":               {552, "Unable to decrypt data"},
+	"cryptAESCantGenerateSessionKey":    {553, "Unable to generate sessionKey"},
+	"keypairCantReadPublicKey":          {600, "Error reading public key"},
+	"keypairCantReadPrivateKey":         {601, "Error reading private key"},
+	"keypairCantGeneratePrivateKey":     {602, "Failed to generate private key"},
+	"keypairCantValidatePrivateKey":     {603, "Failed to validate private key"},
+	"keypairCantEncryptPrivatePEM":      {604, "Failed to encrypt private PEM"},
+	"keypairCantMarshalPublicKey":       {605, "Failed to marshal public key block"},
+	"panicBadErrType":                   {1000, "Bad ErrType"},
+	"panicWTF":                          {1001, "Something has gone horribly wrong."},
 }
 
 // We use this to fail hard, which is a good thing
