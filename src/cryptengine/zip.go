b7b9458e34710b33ab79f8995c4b755b5aa6f2c1
Liam Conrad <liam@storskegg.org>
2017-12-04 17:40:05 -0600

    Initial archive with new dep, and add anti-collision to tmp zip name

--- src/cryptengine/zip.go
+++ src/cryptengine/zip.go
@@ -1,36 +1,39 @@
 package main
 
 import (
-	"errors"
+	//"errors"
 	"fmt"
-	"github.com/jhoonb/archivex"
+	"log"
 	"path"
+	"strconv"
+	"time"
+
+	"github.com/mholt/archiver"
 )
 
 const lcszFileName = "lcsf_secured_files.zip"
 
 func archiveFiles(paths []string) string {
-	archivePath := path.Join(tmpDir(), lcszFileName)
+	archivePath := path.Join(tmpDir(), strconv.FormatInt(time.Now().UnixNano(), 36)+lcszFileName)
 
-	archive := new(archivex.ZipFile)
-	archive.Create(archivePath)
-	defer archive.Close()
-
-	// This will hold the paths of any file we skip
+	// This will hold the paths of any file that we'll add
+	var addedFiles []string
+	// This will hold the paths of any file we'll skip
 	var skippedFiles []string
 
 	for _, path := range paths {
 		fileInfo := pathInfo(path)
 		if !fileInfo.Exists {
-			check(errors.New(errs["fsCantOpenFile"].Msg), errs["fsCantOpenFile"])
-		}
-
-		if fileInfo.IsReg {
-			archive.AddFile(path)
-			fmt.Printf("ZIP::Adding file %s\n", path)
-		} else if fileInfo.IsDir {
-			archive.AddAll(path, false)
-			fmt.Printf("ZIP::Adding directory %s\n", path)
+			//check(errors.New(errs["fsCantOpenFile"].Msg), errs["fsCantOpenFile"])
+			skippedFiles = append(skippedFiles, path)
+		} else if fileInfo.IsDir || fileInfo.IsReg {
+			addedFiles = append(addedFiles, path)
+			//archive.AddFile(path)
+			//fmt.Printf("ZIP::Adding file %s\n", path)
+			//} else if fileInfo.IsDir {
+			//
+			//	archive.AddAll(path, false)
+			//	fmt.Printf("ZIP::Adding directory %s\n", path)
 		} else {
 			skippedFiles = append(skippedFiles, path)
 		}
@@ -38,6 +41,12 @@ func archiveFiles(paths []string) string {
 		for _, spath := range skippedFiles {
 			fmt.Printf("SKIPPED::%s\n", spath)
 		}
+	}
+
+	err := archiver.Zip.Make(archivePath, addedFiles)
+	if err != nil {
+		// TODO: you know the drill...
+		log.Fatalln(err)
 	}
 
 	return archivePath
