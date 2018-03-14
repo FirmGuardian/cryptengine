7a9a8ad3c3402d12c28e1e8cdcbdd709fc955040
Kevin Cisler <kevin.cisler@gmail.com>
2018-03-14 14:01:57 -0500

    added file checks for keygen

--- tempTest/main.go
+++ tempTest/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"fmt"
 	"os/user"
+  "strconv"
 )
 
 
@@ -14,6 +15,11 @@ func main() {
 	password := "password"
 	user, err := user.Current()
 	homeDir := user.HomeDir
+	keyDir := homeDir+"/.LegalCrypt/keys"
+	initFiles := []string{
+	  keyDir + "/id_rsa",
+	  keyDir + "/id_rsa.pub",
+	  keyDir + "/nacl"}
 
 	// keygen and setup
 	output, err := exec.Command("./cryptengine", "-gen", "-t", "rsa", "-p", password, "-eml",
@@ -23,12 +29,12 @@ func main() {
 	}
 	fmt.Println(string(output))
 
+	// check keys exist
+  missingFiles := fileCheck (initFiles)
+  fmt.Println(strconv.Itoa(missingFiles) + " files missing")
+
 	// cleanup
-	output, err = exec.Command("rm", "-rf", homeDir+"/.LegalCrypt").CombinedOutput()
-	if err != nil {
-		os.Stderr.WriteString(err.Error())
-	}
-	fmt.Println(string(output))
+	os.RemoveAll(homeDir+"/.LegalCrypt")
 
 	// check for .LegalCrypt (ls check)
 	output, err = exec.Command("ls", "-a", homeDir).CombinedOutput()
@@ -36,4 +42,21 @@ func main() {
 		os.Stderr.WriteString(err.Error())
 	}
 	fmt.Println(string(output))
+}
+
+// takes a list of file paths and check if the files exists
+// prints when a file path is verified
+// returns # of files it failed to find
+func fileCheck (filepaths []string) (int){
+  failCount := 0
+  for _, path := range filepaths {
+    _, err := os.Stat(path)
+    if !os.IsNotExist(err) {
+      fmt.Println("Found: " + path)
+    } else {
+      fmt.Println("ERROR: Unable to find " + path)
+      failCount++
+    }
+  }
+  return failCount
 }
