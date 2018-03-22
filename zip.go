87b50f4f4c398d035480d031d14b5119e366fe06
Kevin Luty <kevin.charles.luty@jci.com>
2018-03-22 17:03:27 -0500

    Fix code to pass tests
    Important to note the change where we MkdirAll before we zip.  If output dir doesnt exist, code fails

--- zip.go
+++ zip.go
@@ -10,32 +10,40 @@ import (
 	"time"
 
 	"github.com/mholt/archiver"
+  "errors"
 )
 
 const lcszFileName = "lcsf_secured_files"
 
 func archiveFiles(paths []string) string {
+  if len(paths) == 0 {
+    return ""
+  }
+
 	antiCollision := "-" + strconv.FormatInt(time.Now().UnixNano(), 36)
-	archivePath := path.Join(tmpDir(), lcszFileName+antiCollision+".zip")
+	archivePath := path.Join(tmpDir(), lcszFileName + antiCollision + ".zip")
 
 	var addedFiles []string
-	var skippedFiles []string
+	var action string
 
 	for _, path := range paths {
 		fileInfo := pathInfo(path)
-		if !fileInfo.Exists {
-			skippedFiles = append(skippedFiles, path)
-		} else if fileInfo.IsDir || fileInfo.IsReg {
+		if fileInfo.Exists && (fileInfo.IsDir || fileInfo.IsReg) {
 			addedFiles = append(addedFiles, path)
+			action = "ADDED"
 		} else {
-			skippedFiles = append(skippedFiles, path)
+		  action = "SKIPPED"
 		}
-
-		for _, spath := range skippedFiles {
-			fmt.Printf("SKIPPED::%s\n", spath)
-		}
+    fmt.Printf("%s::%s\n", action, path)
 	}
 
+	if len(addedFiles) == 0 {
+	  return ""
+  }
+
+  // We are required to make sure output path exists otherwise archive fails to zip
+  // This could change? https://github.com/mholt/archiver/issues/61
+  os.MkdirAll(tmpDir(), 700)
 	err := archiver.Zip.Make(archivePath, addedFiles)
 	if err != nil {
 		// TODO: you know the drill...
@@ -49,7 +57,7 @@ func unarchiveFiles(zipFile string) error {
 	zInfo := pathInfo(zipFile)
 	if !zInfo.Exists || !zInfo.IsReg {
 		// TODO: You know what to do, here
-		log.Fatalf("Cannot extract! %v is not regular, or file does not exist!\n", zInfo.Clean)
+		return errors.New(fmt.Sprintf("Cannot extract! %v is not regular, or file does not exist!\n", zInfo.Clean))
 	}
 	outPath := path.Join(outDirDec(), strings.Replace(zInfo.File, zInfo.Ext, "", -1))
 	os.MkdirAll(outPath, 0700)
